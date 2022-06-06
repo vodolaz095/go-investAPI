dc4c3327297bad4292bb53e14b5227a0d3f6c92d
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2022-06-06 20:49:29 +0300

    добавлен тест для конвертации цены

--- investapi/helpers_test.go
+++ investapi/helpers_test.go
@@ -18,3 +18,21 @@ func TestQuotationToFloat(t *testing.T) {
 	price := q.ToFloat64()
 	t.Logf("Price %.5f", price)
 }
+
+func Test_MoneyValueFromFloat64(t *testing.T) {
+	/*
+		цена 100.02 для AAPL должна выставляться корректно.
+		Проверьте, что она правильно преобразуется в формат MoneyValue,
+		в котором дробная часть передается в миллиардных долях.
+			100.02 = {units=100,nano=20000000}
+	*/
+
+	v := 100.02
+
+	m := MoneyValueFromFloat64(v)
+	t.Logf("%d %d ", m.Units, m.Nano)
+
+	if m.Units != 100 || m.Nano != 20000000 {
+		t.Fatal("цена не та")
+	}
+}
