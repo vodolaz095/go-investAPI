0d297ce43ffaac55b59e5d68996dc810382f1451
Anatolij Ostroumov <anatolij@vodolaz095.ru>
2022-06-30 20:20:17 +0300

    пример sandbox

---
+++ example/sandbox/sandbox.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"github.com/vodolaz095/go-investAPI/investapi"
+	"log"
+)
+
+/*
+Данный пример создаёт тестовый аккаунт и пополняет его 100 рублями
+*/
+
+const token = "тутДолженБытьТокен" // https://tinkoff.github.io/investAPI/grpc/#tinkoff-invest-api_1
+
+func main() {
+	client, err := investapi.New(token)
+	if err != nil {
+		log.Fatalf("%s : при соединении с investAPI", err)
+	}
+	sandboxService := investapi.NewSandboxServiceClient(client.Connection)
+
+	resp, err := sandboxService.OpenSandboxAccount(context.Background(), &investapi.OpenSandboxAccountRequest{})
+	if err != nil {
+		log.Fatalf("%s : while opening sandbox account", err)
+	}
+	log.Printf("Тестовый счёт %s открыт", resp.AccountId)
+
+	sandboxAccounts, err := sandboxService.GetSandboxAccounts(context.Background(), &investapi.GetAccountsRequest{})
+	if err != nil {
+		log.Fatalf("%s : while getting sandbox accounts", err)
+	}
+	var howMuch investapi.MoneyValue
+	howMuch.Currency = "RUB"
+	for _, account := range sandboxAccounts.GetAccounts() {
+		howMuch = *investapi.MoneyValueFromFloat64(100)
+		_, err = sandboxService.SandboxPayIn(context.Background(), &investapi.SandboxPayInRequest{
+			AccountId: account.Id,
+			Amount:    &howMuch,
+		})
+		if err != nil {
+			log.Fatalf("%s : при попытке пополнить тестовый счёт %s", err, account.Id)
+		}
+	}
+}
